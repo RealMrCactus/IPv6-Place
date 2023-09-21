cde7790680fffe03c8abe5502c0939bb510bbcaf
Bhraedain Teitgen <teitgenbhraedain@gmail.com>
2023-09-21 19:00:12 -0400

    A

--- IPv6-Place/src/main.rs
+++ IPv6-Place/src/main.rs
@@ -10,29 +10,27 @@ use std::net::SocketAddrV6;
 //const WS: &str    = "wss://ssi.place/ws";
 //let image: &str = "out.jpg";
 
-fn process_image(_image: &str) {
-    let image = image::open(_image).unwrap();
+fn process_image(image: &str) {
+    let image = image::open(image).unwrap().into_rgb8();
     
     let socket = Socket::new(Domain::IPV6, Type::RAW, Some(Protocol::ICMPV6)).unwrap();
-    let payload = [0; 8];
-    loop {
-        for (x, y, color) in image.pixels() {
-            // ping format (hex): x, y, r, g, b 2a01:4f8:c012:f8e6:SXXX:YYYY:RR:GGBB s will default to 1
-            let ip = Ipv6Addr::new(
-                0x2a01, 0x4f8, 0xc012, 0xf8e6, 
-                (0x2 << 12) | x as u16, y as u16,
-                color[0] as u16, ((color[1] as u16) << 8) | color[2] as u16
-            );
-            println!("Sending to {}", ip);
-            let addr = SocketAddrV6::new(ip, 1, 0, 0);
-            socket.send_to(&payload, &addr.into()).unwrap();
-        }
+    let payload = [0x80, 0, 0, 0, 0, 0, 0, 0];
+    let _ = socket.set_nonblocking(true);
+    let _ = socket.set_send_buffer_size(usize::MAX);
+    for (x, y, color) in image.enumerate_pixels() {
+        // ping format (hex): x, y, r, g, b 2a01:4f8:c012:f8e6:SXXX:YYYY:RR:GGBB s will default to 1
+        let ip = Ipv6Addr::new(
+            0x2a01, 0x4f8, 0xc012, 0xf8e6, 
+            (0x1 << 12) | x as u16, y as u16,
+            color[0] as u16, ((color[1] as u16) << 8) | color[2] as u16
+        );
+        let addr = SocketAddrV6::new(ip, 1, 0, 0);
+        
+        socket.send_to(&payload, &addr.into()).unwrap();
     }
-    
 }
-
 fn main() {
-    // ping("2a01:4f8:c012:f8e6:f1f9:1c6:32:0000");
-    
-    process_image("src/out.jpg");
+    loop {
+        process_image("out.png");
+    }
 }
