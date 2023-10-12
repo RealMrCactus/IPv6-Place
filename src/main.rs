da0ce87ead96b29e39bea6a46878e82442bd4619
Bhraedain Teitgen <teitgenbhraedain@gmail.com>
2023-10-11 23:20:11 -0400

    a

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn main() {
         for addr in addr_list.iter().cloned() {
            // println!("{}",addr);
             socket.send_to(&payload, &addr.into()).unwrap();
-            std::thread::sleep(time::Duration::from_nanos(0.01));
+            std::thread::sleep(time::Duration::from_nanos(0));
         }
     
     }
