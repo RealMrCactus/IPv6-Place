5a15199051a5e45843ab6b92eef1edf76cc8bf36
rafa_br34 <111343658+rafa-br34@users.noreply.github.com>
2023-10-12 14:37:55 -0300

    Update main.rs
    Better make_address function and use a busy sleep, also add 'targetpps' argument.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(non_snake_case)]
-use std::net::Ipv6Addr;
+use std::{net::Ipv6Addr, process::exit};
 use std::time;
 use clap::Parser;
+use spin_sleep;
 use image;
 use image::GenericImageView;
 use socket2::{Domain, Protocol, Socket, Type};
@@ -14,25 +15,28 @@ use std::net::SocketAddrV6;
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    image: String
+    image: String,
+    #[arg(short, long)]
+    targetpps: u64
+}
+
+fn make_address(size: u8, x: u16, y: u16, r: u8, g: u8, b: u8) -> Ipv6Addr {
+    Ipv6Addr::new(
+        0x2A01, 0x04F8, 0xC012, 0xF8E6,
+		((size as u16) * 0x1000) + x, y, r as u16, ((g as u16) << 8) | (b as u16)
+    )
 }
 
 fn build_addresses(image: String) -> Vec<SocketAddrV6> {
     let mut addrs = Vec::new();
     let image = image::open(image).unwrap().into_rgb8();
     for (x, y, color) in image.enumerate_pixels() {
-        // ping format (hex): x, y, r, g, b 2a01:4f8:c012:f8e6:SXXX:YYYY:RR:GGBB s will default to 1
-        let ip = Ipv6Addr::new(
-            0x2a01,
-            0x4f8,
-            0xc012,
-            0xf8e6,
-            (0x1 << 12) | x as u16,
-            y as u16,
-            color[0] as u16,
-            ((color[1] as u16) << 8) | color[2] as u16,
+        let addr = SocketAddrV6::new(
+            make_address(1, x as u16, y as u16, color[0], color[1], color[2]),
+            0,
+            0,
+            0
         );
-        let addr = SocketAddrV6::new(ip, 0, 0, 0);
         addrs.push(addr);
     }
     addrs
@@ -44,15 +48,19 @@ fn main() {
         
     let addr_list = build_addresses(args.image);
     let socket = Socket::new(Domain::IPV6, Type::RAW, Some(Protocol::ICMPV6)).unwrap();
+    _=socket.set_nonblocking(true);
+    _=socket.set_send_buffer_size(1024 * 1024 * 64);
+
+    let delay = 1_000_000_000 / args.targetpps;
+    println!("Target PPS is {}, using {}ns delay", args.targetpps, delay);
+    // It isn't a good idea to set the sleep accuracy to 100ms, but we'll barely use the native sleep soo
+    let spin_sleeper = spin_sleep::SpinSleeper::new(100_000_000).with_spin_strategy(spin_sleep::SpinStrategy::SpinLoopHint);
     let payload = [0x80, 0, 0, 0, 0, 0, 0, 0];
-    let _ = socket.set_nonblocking(false);
-    let _ = socket.set_send_buffer_size(1024 * 1024 * 64);
     loop {
         for addr in addr_list.iter().cloned() {
            // println!("{}",addr);
             socket.send_to(&payload, &addr.into()).unwrap();
-            std::thread::sleep(time::Duration::from_nanos(0));
+            spin_sleeper.sleep_ns(delay);
         }
-    
     }
 }
